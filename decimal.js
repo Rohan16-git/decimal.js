433e4717d57588818017329ed8bde3e9a084d5e1
Rohan16-git <roh@angithub16>
2024-11-16 15:28:01 +0530

    new

---
+++ decimal.js
@@ -0,0 +1,12 @@
+var Array1 = [2, 4, 6, 3, 1, 5, 9, 8]
+console.log( "Original Array", Array1)
+
+Array1.push(1.33, 2.1, 3.14, 32.55)
+console.log("New Array", Array1)
+
+const result = Array1.filter(num =>Number.isInteger(num) )
+const result1 = Array1.filter(num => !Number.isInteger(num) )
+
+console.log("New Array",Array1)
+console.log("Interger", result)
+console.log("Decimals", result1)
